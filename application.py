4024ec5e8d8fcad8874e176a5c28bf18f9a7fc04
Samus_ <mail2samus@gmail.com>
2009-10-15 04:17:22 -0200

    integrating first state and 'change_state' method

--- application.py
+++ application.py
@@ -5,31 +5,50 @@ from os import path
 pygtk.require("2.0")
 
 def reparent(manager, template, container):
+    # construye en nuevo contenido a partir del template de Glade
     builder = gtk.Builder()
     builder.add_from_file(path.join("templates", template))
+    # conecta señales a la clase controladora
     builder.connect_signals(manager)
 
+    # elimina contenido previo si existe
+    for child in container.get_children():
+        container.remove(child)
+
+    # inserta el nuevo contenido en el viejo contenedor
     window = builder.get_object("window")
     for child in window.get_children():
         child.reparent(container)
 
+    return builder
+
+class Inicio(object):
+    template = "inicio.glade"
+
 class ApplicationManager(object):
     """ Maquina de estados principal
 
-    -Inicializa contenido de la ventana (marco) tanto para Sugar como Standalone
-    -Provee el metodo 'change_state' utilizado para cargar un nuevo bloque dentro del marco
-    -Provee atributo 'state_info' (diccionario compartido que almacena los datos de cada contenido cargado)
+    -Inicializa el contenido exterior de la ventana (marco) tanto para Sugar como Standalone, para ello
+     recibe una instancia de gtk.Window como argumento del constructor.
+    -Provee el metodo 'change_state' utilizado para cargar un nuevo bloque interior dentro del marco.
+    -Provee atributo 'state_info' (diccionario compartido que almacena los datos de cada contenido cargado).
     """
-    def __init__(self, frame_container):
+    def __init__(self, window):
         super(ApplicationManager, self).__init__()
 
-        reparent(self, "frame.glade", frame_container)
+        builder = reparent(self, "frame.glade", window)
+        self.inner_container = builder.get_object("inner_container")
 
-        #self.state_info = dict()
-        #self.change_state(Inicio(self))
+        window.connect("destroy", self.gtk_main_quit)
+        window.set_title("Fracciones")
 
-        frame_container.connect("destroy", self.gtk_main_quit)
-        frame_container.show()
+        self.state_info = dict()
+        self.change_state(Inicio)
+
+        window.show()
+
+    def change_state(self, state_class):
+        reparent(state_class(), state_class.template, self.inner_container)
 
     def gtk_main_quit(self, userdata=None):
         gtk.main_quit()
