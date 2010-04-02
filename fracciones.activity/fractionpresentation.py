81f2522c98458147802ae2a1ec3b929c497ff152
jmpc <jumapico@gmail.com>
2010-04-02 10:48:56 -0300

    Added internationalization.

--- fracciones.activity/fractionpresentation.py
+++ fracciones.activity/fractionpresentation.py
@@ -6,6 +6,8 @@ Contain the presentation of the game, a widget descendant from container.
 import logging
 import gtk
 import pango
+import gettext
+from gettext import gettext as _
 
 import gtkcake
 from fractionlogic import FractionLogic
@@ -18,6 +20,10 @@ log = logging.getLogger()
 class FractionPresentation(gtk.VBox):
     def __init__(self):
         super(FractionPresentation, self).__init__()
+
+        gettext.bindtextdomain("fracciones", "./locale")
+        gettext.textdomain("fracciones")
+
         # Change font size
         #settings = gtk.settings_get_default()
         #settings.set_string_property("gtk-font-name", "Sans 40", "FractionPresentation")
@@ -30,33 +36,33 @@ class FractionPresentation(gtk.VBox):
         # 1. Add the menu bar
         mb = gtk.MenuBar()
         self.pack_start(mb, False, False, 0)
-        
+
         # game menu
         gamemenu = gtk.Menu()
-        gamem = gtk.MenuItem("Game")
+        gamem = gtk.MenuItem(_("Game"))
         gamem.set_submenu(gamemenu)
 
-        new = gtk.MenuItem("New")
+        new = gtk.MenuItem(_("New"))
         new.connect("activate", self.menu_game_new)
         gamemenu.append(new)
 
-        exit = gtk.MenuItem("Exit")
+        exit = gtk.MenuItem(_("Exit"))
         exit.connect("activate", self.menu_game_exit)
         gamemenu.append(exit)
 
         mb.append(gamem)
         # help menu
         helpmenu = gtk.Menu()
-        helpm = gtk.MenuItem("Help")
+        helpm = gtk.MenuItem(_("Help"))
         helpm.set_submenu(helpmenu)
 
-        about = gtk.MenuItem("About")
+        about = gtk.MenuItem(_("About"))
         about.connect("activate", self.menu_help_about)
         helpmenu.append(about)
 
         mb.append(helpm)
         # 2. label
-        label = gtk.Label("""Eat %i/%i"""%self.logic.get_current())
+        label = gtk.Label(_("""Eat %i/%i""") % self.logic.get_current())
         self.pack_start(label, False, True)
         self.label = label
         # 3. cake
@@ -66,7 +72,7 @@ class FractionPresentation(gtk.VBox):
         self.pack_start(aspect)
         self.cake = cake
         # 4. button
-        button = gtk.Button("Check")
+        button = gtk.Button(_("Check"))
         button.connect("clicked", self.check_cake)
         self.pack_start(button, False, True)
 
@@ -83,7 +89,7 @@ class FractionPresentation(gtk.VBox):
         log.debug("menu_game_exit")
         gtk.main_quit()
 
-    
+
     def menu_help_about(self, menuitem):
         print menuitem
         log.debug("menu_help_about")
@@ -97,17 +103,17 @@ class FractionPresentation(gtk.VBox):
         """Clicked button check"""
         log.debug("on_clicked_check")
         if self.logic.is_equal(self.cake.current_fraction):
-            md = gtk.MessageDialog(None, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, "GOOD!")
+            md = gtk.MessageDialog(None, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, _("GOOD!"))
             md.run()
             md.destroy()
             self.new_game()
         else:
-            md = gtk.MessageDialog(None, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, "BAD!")
+            md = gtk.MessageDialog(None, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, _("BAD!"))
             md.run()
             md.destroy()
 
 
     def new_game(self):
         self.logic.generate()
-        self.label.set_text("""Eat %i/%i"""%self.logic.get_current())
+        self.label.set_text(_("""Eat %i/%i""") % self.logic.get_current())
         self.cake.reset(self.logic.get_current()[1])
